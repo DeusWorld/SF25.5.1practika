ebd9793e27c5261092a1c05447d89c550d5defd8
Deus <115874062+DeusWorld@users.noreply.github.com>
2023-03-15 16:52:10 +0300

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -9,7 +9,6 @@ from selenium.webdriver.common.by import By
 def testing():
    pytest.driver = webdriver.Chrome('/web/chromedriver.exe')
 
-   # Переходим на страницу авторизации
    pytest.driver.get('https://petfriends.skillfactory.ru/login')
 
    yield
@@ -20,17 +19,13 @@ def testing():
 def go_to_my_pets():
 
    element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.ID, "email")))
-   # Вводим email
    pytest.driver.find_element_by_id('email').send_keys(valid_email)
 
    element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.ID, "pass")))
-   # Вводим пароль
    pytest.driver.find_element_by_id('pass').send_keys(valid_password)
 
    element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[type='submit']")))
-   # Нажимаем на кнопку входа в аккаунт
    pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
 
    element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Мои питомцы")))
-   # Нажимаем на ссылку "Мои питомцы"
    pytest.driver.find_element_by_link_text("Мои питомцы").click()
